3e3fccd5aaf7596e94735cf80986005eb8f372cf
coder11011 <116571097+coder11011@users.noreply.github.com>
2022-10-24 19:17:31 +0530

    Update multi.c

--- C/multi.c
+++ C/multi.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, sum;
+    int number1, number2, mult;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     
-    sum = number1 * number2;      
+    mult = number1 * number2;      
     
-    printf("%d X %d = %d", number1, number2, sum);
+    printf("%d X %d = %d", number1, number2, mult);
     return 0;
 }
